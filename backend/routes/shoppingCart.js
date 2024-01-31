45b871f4498f081b7adcc3ceadbe957c525d926b
LizzieDworsky <amylizzieroyal@gmail.com>
2024-01-31 11:14:33 -0700

    added and tested endpoints for create and read on shopping cart, filtered by username

--- backend/routes/shoppingCart.js
+++ backend/routes/shoppingCart.js
@@ -5,9 +5,17 @@ const {
     validateShoppingCart,
 } = require("../models/shoppingCart");
 
-router.get("/", async (req, res) => {
+router.get("/:username", async (req, res) => {
     try {
-        res.status(200).send("Get all Request.");
+        let user = req.params.username;
+        let shoppingCart = await ShoppingCart.find({ user: user }).populate(
+            "product"
+        );
+        if (shoppingCart.length > 0) {
+            return res.status(200).send(shoppingCart);
+        } else {
+            return res.status(404).send("Cart not found.");
+        }
     } catch (error) {
         res.status(500).send(`Internal Server Error ${error}`);
     }
@@ -15,7 +23,15 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
     try {
-        res.status(201).send("Post Request.");
+        let { error } = validateShoppingCart(req.body);
+        if (error) {
+            return res.status(400).send("Invalid body for post request.");
+        }
+
+        let newShoppingCart = new ShoppingCart(req.body);
+        await newShoppingCart.save();
+
+        res.status(201).send(newShoppingCart);
     } catch (error) {
         res.status(500).send(`Internal Server Error ${error}`);
     }
