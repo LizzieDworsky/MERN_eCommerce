11a624c5194607e4195375530bb4655418a946ad
LizzieDworsky <amylizzieroyal@gmail.com>
2024-01-31 08:05:07 -0700

    added project boilerplate code and connected to MongoDb

---
+++ backend/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDb = () => {
+    mongoose.set("strictQuery", true);
+    mongoose
+        .connect(process.env.CONNECTION_STRING)
+        .then(() => console.log("Connected to MongoDb..."))
+        .catch((err) => {
+            console.log(`Could not connect to MongoDb. Error: ${err}`);
+            process.exit(1);
+        });
+};
+
+module.exports = connectDb;
