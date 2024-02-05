9b037c3a230a3462cf2390ca571d3f32e676d528
LizzieDworsky <amydworsky15@gmail.com>
2024-02-05 16:04:50 -0700

    adjusted placeholder photos to alternate + minor adjustments to styling for product cards

--- frontend/src/components/Products/ProductCard.jsx
+++ frontend/src/components/Products/ProductCard.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
-import placeholderImage from "../../images/placeholder_cat_mom.png";
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, placeholder }) => {
     return (
         <Link>
             <div className="product-card">
-                <img src={placeholderImage} alt="Product" />
+                <img src={placeholder} alt="Product" />
                 <h4>{product.name}</h4>
             </div>
         </Link>
--- frontend/src/components/Products/ProductsList.jsx
+++ frontend/src/components/Products/ProductsList.jsx
@@ -1,11 +1,21 @@
 import ProductCard from "./ProductCard";
+import placeholderImageOne from "../../images/placeholder_cat_mom.png";
+import placeholderImageTwo from "../../images/placeholder_candle.png";
 import "./Products.css";
 
 const ProductsList = ({ products }) => {
+    const getPlaceholderImage = (index) => {
+        return index % 2 === 0 ? placeholderImageOne : placeholderImageTwo;
+    };
+
     return (
         <div className="product-list">
-            {products.map((product) => (
-                <ProductCard key={product._id} product={product} />
+            {products.map((product, index) => (
+                <ProductCard
+                    key={product._id}
+                    product={product}
+                    placeholder={getPlaceholderImage(index)}
+                />
             ))}
         </div>
     );
