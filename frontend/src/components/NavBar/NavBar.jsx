7b7885f12b82d3578d15402654a9d8baccfcbba6
LizzieDworsky <amydworsky15@gmail.com>
2024-02-05 14:20:59 -0700

    added route links to NavBar component

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-    return <div className="nav-bar">Nav Bar</div>;
+    return (
+        <div className="nav-bar">
+            <Link to={"/"}>
+                <h1>Amy's Shop</h1>
+            </Link>
+            <ul>
+                <li>
+                    <Link to={"cart"}>Cart</Link>
+                </li>
+                <li>
+                    <Link to={"login"}>Login</Link>
+                </li>
+                <li>
+                    <Link to={"register"}>Register</Link>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default NavBar;
