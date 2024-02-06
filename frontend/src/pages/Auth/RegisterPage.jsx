64cc6e7e2bbf274f291e612f636014b5c941eacd
LizzieDworsky <amydworsky15@gmail.com>
2024-02-06 11:03:50 -0700

    added html structure to registration page

--- frontend/src/pages/Auth/RegisterPage.jsx
+++ frontend/src/pages/Auth/RegisterPage.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const RegisterPage = () => {
-    return <div>Register Page</div>;
+    const [credentials, setCredentials] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(credentials);
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label>Enter your username:</label>
+                <input name="username" type="text" />
+                <label>Enter your email:</label>
+                <input name="email" type="email" />
+                <label>Enter your password:</label>
+                <input name="password" type="password" />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
 };
 
 export default RegisterPage;
