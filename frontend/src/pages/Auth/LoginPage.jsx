43773c6f091c5fddbbef9ef0f31e7a5143a761d1
LizzieDworsky <amydworsky15@gmail.com>
2024-02-07 11:04:12 -0700

    added logic to handle input for credentials

--- frontend/src/pages/Auth/LoginPage.jsx
+++ frontend/src/pages/Auth/LoginPage.jsx
@@ -1,6 +1,22 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../utils/useAuth";
+
 const LoginPage = () => {
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({ ...credentials, [name]: value });
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(credentials);
     };
 
     return (
@@ -8,9 +24,19 @@ const LoginPage = () => {
             <h2>Login Form</h2>
             <form onSubmit={(e) => handleSubmit(e)}>
                 <label>Enter your username:</label>
-                <input type="text" />
+                <input
+                    name="username"
+                    type="text"
+                    value={credentials.username}
+                    onChange={(e) => handleChange(e)}
+                />
                 <label>Enter your password:</label>
-                <input type="password" />
+                <input
+                    name="password"
+                    type="password"
+                    value={credentials.password}
+                    onChange={(e) => handleChange(e)}
+                />
                 <button type="submit">Login</button>
             </form>
         </div>
